ca83ba94d18eaf2e36893e0762a3d8bf97af7c0f
jkoller90 <jmk4411@gmail.com>
2017-02-25 00:39:40 -0500

    need to add media queries

--- greyCircle/grey.js
+++ greyCircle/grey.js
@@ -1,45 +1,53 @@
 var x_x;
-var context; 
-
+var context;
 //scoreboard
 var hitText = 'Hits: ';
-var hits = 0; 
+var hits = 0;
 var timeText = "Time: ";
-var time = 60; 
-
-
-//var timer = setInterval(function(){ 
-//		if(time > -1){
-//			$("#time").html(timeText + time--); 			
-//		}else if(time == -1){
-//			alert("Time's up!");
-//			clearInterval(timer);
-//		} 
-//	}, 1000);
-
-//if(time == -1) 
-
-
-$(window).on("load resize", function() {
-windowWidth = $(window).height();    
- windowHeight = $(window).width();
-//$("body").attr("background-size", ""+windowHeight+"px "+windowWidth+"px;");    
+var time = 60;
+$(window).bind('orientationchange resize', function (event) {
+	if (event.orientation) {
+		if (event.orientation == 'landscape') {
+			if (window.rotation == 90) {
+				rotate(this, -90);
+			}
+			else {
+				rotate(this, 90);
+			}
+		}
+	}
 });
 
-
+function rotate(el, degs) {
+	iedegs = degs / 90;
+	if (iedegs < 0) iedegs += 4;
+	transform = 'rotate(' + degs + 'deg)';
+	iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation=' + iedegs + ')';
+	styles = {
+		transform: transform
+		, '-webkit-transform': transform
+		, '-moz-transform': transform
+		, '-o-transform': transform
+		, filter: iefilter
+		, '-ms-filter': iefilter
+	};
+	$(el).css(styles);
+}
+//$(window).on("load resize", function () {
+//	windowWidth = $(window).height();
+//	windowHeight = $(window).width();
+//});
 
 function circleUI() {
 	this.mousedown = false;
 	this.canvasEl = $('#canvas');
 	this.canvas = document.getElementById("canvas");
 	context = this.canvas.getContext('2d');
-	this.canvasEl.attr("id","drawing");
+	this.canvasEl.attr("id", "drawing");
 	this.init();
 }
 circleUI.prototype.init = function () {
-	$(window).bind("mousedown touchstart",
-		this.showControl.bind(this)
-	);
+	$(window).bind("mousedown touchstart", this.showControl.bind(this));
 }
 var redoptions = {
 	canvasWidth: 400
@@ -49,93 +57,79 @@ var redoptions = {
 		, strokeColor: '#CC1E1E'
 	}
 };
-
 var drawSwitch = false;
 circleUI.prototype.draw = function () {
-//	console.log(x_x + " <- x_x");
-if(!isNaN(x_x)){
-	if (x_x > .8) {
-		$("#hits").html(hitText + ++hits);
-		drawSwitch = true;
-		$("div").append("<canvas id='drawing' height='400' width='400'></canvas>");
-		this.canvas = document.getElementById("drawing");
-		this.ctx = this.canvas.getContext('2d');
-		this.circ = Math.PI * 2;
-		this.quart = Math.PI / 2;
-		this.options = redoptions;
-		this.ctx.lineWidth = this.options.strokeWidth;
-		this.canvas.removeAttribute("id");	
+		if (x_x > .8 && !isNaN(x_x)) {
+			$("#hits").html(hitText + ++hits);
+			drawSwitch = true;
+			$("div").append("<canvas id='drawing' height='400' width='400'></canvas>");
+			this.canvas = document.getElementById("drawing");
+			this.ctx = this.canvas.getContext('2d');
+			this.circ = Math.PI * 2;
+			this.quart = Math.PI / 2;
+			this.options = {
+	canvasWidth: 400
+	, strokeWidth: 3.0
+	, y: {
+		radius: 20
+		, strokeColor: '#CC1E1E'
 	}
-	else {
-		if(drawSwitch){
-			drawSwitch=false;
+};
+			this.ctx.lineWidth = this.options.strokeWidth;
+			this.canvas.removeAttribute("id");
+			console.log(redoptions);
 		}
-		this.canvas = document.getElementById("drawing");
-		this.ctx = this.canvas.getContext('2d');
-		this.circ = Math.PI * 2;
-		this.quart = Math.PI / 2;
-		this.options = {
-			canvasWidth: 400
-			, strokeWidth: 3.0
-			, y: {
-				radius: 20
-				, strokeColor: 'rgba(192, 192, 192, 1)'
+		else {
+			if(!isNaN(x_x)){				
+			if (drawSwitch) {
+				drawSwitch = false;
 			}
-		};	
-		this.ctx.lineWidth = this.options.strokeWidth;
-		setTimeout(function(){$("#drawing").attr("class","inactive")}, 250);
-		$("#drawing").attr("class","active");		
-	}
-    
-	var x = this.x / $(window).width();
-	var y = this.y / $(window).height();
-	var ctx = this.ctx;	
-	var center = this.options.canvasWidth / 2;
-	ctx.drawImage(this.canvas, 0, 0);
-	ctx.beginPath();
-	ctx.arc(center, center, this.options.y.radius, -this.quart, (this.circ * y) - this.quart, false);
-	ctx.strokeStyle = this.options.y.strokeColor;
-	ctx.stroke();
-	// Y filler
-	ctx.beginPath();
-	ctx.arc(center, center, this.options.y.radius, (this.circ * y) - this.quart, -this.quart, false);
-	ctx.strokeStyle = this.options.strokeColor;
-	ctx.stroke();
-	setTimeout(function(){
-		$("#drawing").attr("class","");
-	}, 300);  
-    
-}else{
-    return;
-}    
+			this.canvas = document.getElementById("drawing");
+			this.ctx = this.canvas.getContext('2d');
+			this.circ = Math.PI * 2;
+			this.quart = Math.PI / 2;
+			this.options = {
+				canvasWidth: 400
+				, strokeWidth: 3.0
+				, y: {
+					radius: 20
+					, strokeColor: 'rgba(192, 192, 192, 1)'
+				}
+			};
+			this.ctx.lineWidth = this.options.strokeWidth;
+			setTimeout(function () {
+				$("#drawing").attr("class", "inactive")
+			}, 250);
+			$("#drawing").attr("class", "active");
+		}else{return;}
+		}
+		var x = this.x / $(window).width();
+		var y = this.y / $(window).height();
+		var ctx = this.ctx;
+		var center = this.options.canvasWidth / 2;
+		ctx.drawImage(this.canvas, 0, 0);
+		ctx.beginPath();
+		ctx.arc(center, center, this.options.y.radius, -this.quart, (this.circ * y) - this.quart, false);
+		ctx.strokeStyle = this.options.y.strokeColor;
+		ctx.stroke();
+		// Y filler
+		ctx.beginPath();
+		ctx.arc(center, center, this.options.y.radius, (this.circ * y) - this.quart, -this.quart, false);
+		ctx.strokeStyle = this.options.strokeColor;
+		ctx.stroke();
+		setTimeout(function () {
+			$("#drawing").attr("class", "");
+		}, 220);
+	
 }
-
-var x_offset;
-$("html").on("click",function(e){
-//    x_offset = $(this).offset();
-    x_offset = e.clientX/windowWidth;
-    console.log(x_offset);
-})
-
 circleUI.prototype.showControl = function (e) {
-//  x_offset = this.offset().left;
-		this.x = e.pageX;
-//  console.log(e.pageX - x_offset);
-    
-		x_x = this.x / $("body").width();
-		this.y = e.pageY;
-  if(!isNaN(x_x)){
-		$("#drawing").addClass('active').css({
-			top: this.y
-			, left: this.x
-		});
-
-
-			this.draw();
-      
-  }
+    this.x = e.clientX;
+    x_x = this.x / $("body").width();
+    this.y = e.pageY;
+    $("#drawing").addClass('active').css({
+        top: this.y
+        , left: this.x
+    });
+    this.draw();
 }
-
-
-
 new circleUI();
