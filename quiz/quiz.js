710b754b0cba8534ae16862c56ef740a0d2710b7
jkoller90 <jmk4411@gmail.com>
2017-03-16 15:51:35 -0400

    quiz updated with style specifications

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,7 +1,7 @@
+/* public Object[] */
 function getRandomOrder(choices) {
 	var used = [];
 	var randomized = [];
-	var used = [];
 	var l = 0;
 	while (l < choices.length) {
 		var y = Math.floor((Math.random() * choices.length));
@@ -13,7 +13,10 @@ function getRandomOrder(choices) {
 	}
 	return randomized;
 }
-
+/*Object[]*/
+var randomized_questions = getRandomOrder(questions);
+renderQuiz(randomized_questions);
+/*public String*/
 function renderQuestion(question) {
 	var questionId = questions.indexOf(question);
 	var render_string = "<div class='question' id='" + questionId + "'><h2>" + questions[questionId].q + "</h2>";
@@ -24,7 +27,7 @@ function renderQuestion(question) {
 	render_string += "</div>";
 	return render_string;
 }
-
+/*public void*/
 function renderQuiz(randomized_questions) {
 	for (var i = 0; i < 4; i++) {
 		//	for (var i = 0; i < randomized_questions.length; i++) { 
@@ -32,71 +35,77 @@ function renderQuiz(randomized_questions) {
 		var appendStr = renderQuestion(randomized_questions[i]);
 		$("#quiz").append(appendStr);
 	}
-  $("#quiz").append('<div id="next">Next </div>')    
+	$("#quiz").append('<div id="next">Next </div>')
 }
-var randomized_questions = getRandomOrder(questions);
-renderQuiz(randomized_questions);
-//final code is a mechanism to swap properly answered questions for new questions
+/* int */
 var score = 0;
+/* domObject */
 var ObjDivArray = document.getElementsByClassName("question");
+/* int */
 var i = 0;
+/* int */
 var currId = ObjDivArray[i].id;
 $("#" + currId).css("display", "block");
-var myBarValue = 0; //red bar = 0
+/*int*/
+var myBarValue = 0;
 $(".choice").click(function () {
-	console.log(i);
 	//right answer 
 	if ($(this).text() == $(this).attr("answer")) {
-		 i++;
+		i++;
 		//give white highlight, blue text
-    $(this).attr("style","background-color:white; color: rgba(0, 10, 20, .85);");
-		$("#next").attr("style", "display: block");
-     ++score; 
-  $(".score").text(score+" of 4");
-  myBarValue+=25;
-  $("#myBar").attr("style", "height: 20px;background-color: red; width:"+(myBarValue+"%;"));
-
-  $("#next").click(function(){
-		console.log(i, score);
-		$("#next").attr("style", "display: none");
-		//makes current question disappear
-		$("#" + currId).attr("style", "display: none");
-		if(i !== 4){
-		currId = ObjDivArray[i].id;
-		(function () {
-			var currId = ObjDivArray[i].id;
-			$("#" + currId).css("display", "block");
-		})();			
-		}else{//can report score in modifications 
-			$("#results").html("<h1>Great job!<a href='quiz.html'>Play again? </a></h1>");
-			$("#results").css("display","block");
-		}		
-		console.log(i, score);
-	});       
+		$(this).attr("style", "background-color:white; color: rgba(0, 10, 20, .85);");
+		setTimeout(function () {
+			$("#next").attr("style", "display: block");
+			++score;
+			$(".score").text(score + " of 4");
+			myBarValue += 25;
+			$("#myBar").attr("style", "height: 20px;background-color: red; width:" + (myBarValue + "%;"));
+			$("#next").click(function () {
+				$("#next").attr("style", "display: none");
+				//makes current question disappear
+				$("#" + currId).attr("style", "display: none");
+				if (i !== 4) {
+					currId = ObjDivArray[i].id;
+					(function () {
+						var currId = ObjDivArray[i].id;
+						$("#" + currId).css("display", "block");
+					})();
+				}
+				else { //can report score in modifications 
+					$("#results").html("<h1>Great job!<br><a href='quiz.html'>Play again? </a></h1>");
+					$("#results").css("display", "block");
+				}
+			});
+		}, 500);
 	}
+	//wrong answer
 	else {
-     alert("Please try again.");
+		$(this).attr("style", "background-color:white; color: rgba(0, 10, 20, .85);");
+		var x = $(this); //placeholder because "this" changes in setTimeout callback
+		setTimeout(function () {
+			alert("Please try again.");
+			x.attr("style", "background-color:rgba(0,0,0,0); color: white;");
+		}, 500);
 	}
-
-	
 });
-
-$("#understood").click(function(){
-	$("#info_splash").css("display","none");
-	$("#quiz").css("display","block");
- $(".header").append('<div class="score">0 of 4</div><div id="myProgress"><div id="myBar"></div></div>');
+$("#understood").click(function () {
+	$("#info_splash").css("display", "none");
+	$("#quiz").css("display", "block");
+	$(".header").append('<div class="score">0 of 4</div><div id="myProgress"><div id="myBar"></div></div>');
 });
-
+/*public void*/
 function move() {
-    var elem = document.getElementById("myBar"); 
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++; 
-            elem.style.width = width + '%'; 
-        }
-    }
+	var elem = document.getElementById("myBar");
+	var width = 1;
+	var id = setInterval(frame, 10);
+	/*private void*/
+	function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+		}
+		else {
+			width++;
+			elem.style.width = width + '%';
+		}
+	}
 }
