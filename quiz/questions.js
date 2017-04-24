c43f3ae58954a38fd396d8162afb40e807eab75b
Jon Koller <jmk4411@gmail.com>
2017-04-24 18:07:33 -0400

    quiz questsions updated, updated functionality for converting json; slideshow updated

--- quiz/questions.js
+++ quiz/questions.js
@@ -1,96 +1,163 @@
-var questions = [{
-	"title": "Origin Story"
-	, "q": "Red Stripe has been interwoven with Jamaica's culture since its beginning. In what year was the first Red Stripe branded beer brewed?"
-	, "a": 1928
-	, "choices": [1928, 1918, 1935]
+var docsheet = [
+	{
+		'title': "Origin Story"
+		, 'q': "Red Stripe has been interwoven with Jamaica's culture since its beginning. In what year was the first Red Stripe branded beer brewed?"
+		, 'a': 1928
+		, 'choices': "[1928, 1918, 1935]"
 }, {
-	"title": "What Kind of a Bottle is This?"
-	, "q": "True or False: The iconic shape of the Red Stripe bottle is called a stubby."
-	, "a": "True"
-	, "choices": ["True", "False"]
+		'title': "What Kind of a Bottle is This?"
+		, 'q': "True or False: The iconic shape of the Red Stripe bottle is called a stubby."
+		, 'a': "True"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "'Tis a Ruby Treat"
-	, "q": "True or False: Red Stripe offers a Christmas lager that capitalizes on the sweet and spicy taste of sorrel, a Jamaican favorite."
-	, "a": "True"
-	, "choices": ["True", "False"]
+		'title': "'Tis a Ruby Treat"
+		, 'q': "True or False: Red Stripe offers a flavored lager that capitalizes on the sweet and spicy taste of sorrel, a Jamaican favorite."
+		, 'a': "True"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "One Love"
-	, "q": "True or False: Portmore, Jamaica is home to a museum dedicated to famous Jamaican singer Bob Marley."
-	, "a": "False"
-	, "choices": ["True", "False"]
+		'title': "One Love"
+		, 'q': "True or False: Portmore, Jamaica is home to a museum dedicated to famous Jamaican singer Bob Marley."
+		, 'a': "False"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "It's Been Too Long..."
-	, "q": "In September 2016, Red Stripe celebrated the shipment of the first container of its beer to be exported to the U.S. in how many years?"
-	, "a": 4
-	, "choices": [4, 10, 6]
+		'title': "It's Been Too Long..."
+		, 'q': "In September 2016, Red Stripe celebrated the shipment of the first container of its beer to be exported to the U.S. in how many years?"
+		, 'a': 4
+		, 'choices': '[4, 10, 6]'
 }, {
-	"title": "The Spirit of a Brew"
-	, "q": "Red Stripe embodies"
-	, "a": "All of these"
-	, "choices": ["All of these", "Good vibes", "Jamaican pride"]
+		'title': "The Spirit of a Brew"
+		, 'q': "Red Stripe embodies"
+		, 'a': "All of these"
+		, 'choices': '["All of these", "Good vibes", "Jamaican pride"]'
 }, {
-	"title": "What's the word?"
-	, "q": "True or False: Jamaica has its own local dialect called Patois."
-	, "a": "True"
-	, "choices": ["True", "False"]
+		'title': "What's the word?"
+		, 'q': "True or False: Jamaica has its own local dialect called Patois."
+		, 'a': "True"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "What's in a Name?"
-	, "q": "Which of the following is not a nickname for Jamaica?"
-	, "a": "The \"Island\""
-	, "choices": ["The \"Island\"", "The \"Rock\"", "Jamdown"]
+		'title': "What's in a Name?"
+		, 'q': "Which of the following is not a nickname for Jamaica?"
+		, 'a': 'The "Island"'
+		, 'choices': '["The \\\"Island\\\"", "The \\\"Rock\\\"", "\\\"Jamdown\\\""]'
 }, {
-	"title": "Distinguished Colors"
-	, "q": "True or False: Red Stripe gets its name from the red stripes on the Jamaican flag"
-	, "a": "False"
-	, "choices": ["False", "True"]
+		'title': "Distinguished Colors"
+		, 'q': "True or False: Red Stripe gets its name from the red stripes on the Jamaican flag"
+		, 'a': "False"
+		, 'choices': '["False", "True"]'
 }, {
-	"title": "The Bounty of Independence Calls For a Toast"
-	, "q": "Many Red Stripes were consumed in this year, in celebration of Jamaica gaining independence."
-	, "a": 1962
-	, "choices": [1962, 1958, 1953]
+		'title': "The Bounty of Independence Calls For a Toast"
+		, 'q': "Many Red Stripes were consumed in this year, in celebration of Jamaica gaining independence."
+		, 'a': 1962
+		, 'choices': '[1962, 1958, 1953]'
 }, {
-	"title": "Green and Red with a Dark, Yellow Brew?"
-	, "q": "True or False: Red Stripe first arrived in the United States in green bottles instead of their iconic squat brown bottles."
-	, "a": "True"
-	, "choices": ["True", "False"]
+		'title': "Green and Red with a Dark, Yellow Brew?"
+		, 'q': "True or False: Red Stripe first arrived in the United States in green bottles instead of their iconic squat brown bottles."
+		, 'a': "True"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "Lord of the Land"
-	, "q": "What is the capital of Jamaica?"
-	, "a": "Kingston"
-	, "choices": ["Montego Bay", "Kingston", "Port Antonio"]
+		'title': "Lord of the Land"
+		, 'q': "What is the capital of Jamaica?"
+		, 'a': "Kingston"
+		, 'choices': '["Montego Bay", "Kingston", "Port Antonio"]'
 }, {
-	"title": "Flavor With a Kick"
-	, "q": "True or False: Jamaica is famous for its jerk spice, which is an integral part of their cuisine."
-	, "a": "True"
-	, "choices": ["True", "False"]
+		'title': "Flavor With a Kick"
+		, 'q': "True or False: Jamaica is famous for its jerk spice, which is an integral part of their cuisine."
+		, 'a': 'True'
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "From Kingston With Love"
-	, "q": "True or False: While some of the James Bond novels cite Jamaica as the setting, none of the movies have been filmed on the island."
-	, "a": "False"
-	, "choices": ["True", "False"]
+		'title': "From Kingston With Love"
+		, 'q': "True or False: While some of the James Bond novels cite Jamaica as the setting, none of the movies have been filmed on the island."
+		, 'a': "False"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "An Important Distinction"
-	, "q": "True or False: Similar to the U.S., Jamaica drivers drive on the right side of the road."
-	, "a": "False"
-	, "choices": ["True", "False"]
+		'title': "An Important Distinction"
+		, 'q': "True or False: Similar to the U.S., Jamaica drivers drive on the right side of the road."
+		, 'a': "False"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "Mind the Off-Beat"
-	, "q": "True or False: Reggae music is driven by the piano and drums."
-	, "a": "False"
-	, "choices": ["True", "False"]
+		'title': "Mind the Off-Beat"
+		, 'q': "True or False: Reggae music is driven by the piano and drums."
+		, 'a': "False"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "A Recipe for Royalty"
-	, "q": "True or False: Red Stripe is brewed with just three ingredients."
-	, "a": "False"
-	, "choices": ["True", "False"]
+		'title': "A Recipe for Royalty"
+		, 'q': "True or False: Red Stripe is brewed with just three ingredients."
+		, 'a': "False"
+		, 'choices': '["True", "False"]'
 }, {
-	"title": "A Recipe for Royalty"
-	, "q": "Red Stripe introduced its stubby bottle to Jamaica in what year?"
-	, "a": 1965
-	, "choices": [1965, 1928, 1938]
+		'title': "A Recipe for Royalty"
+		, 'q': "Red Stripe introduced its stubby bottle to Jamaica in what year?"
+		, 'a': 1965
+		, 'choices': "[1965, 1928, 1938]"
 }, {
-	"title": "Vital Roots"
-	, "q": "The following music genres all originated in Jamaica, except"
-	, "a": "Hip-Hop"
-	, "choices": ["Hip-Hop", "Ska", "Reggae"]
-}]
+		'title': "Vital Roots"
+		, 'q': "The following music genres all originated in Jamaica, except"
+		, 'a': "Hip-Hop"
+		, 'choices': '["Hip-Hop", "Ska", "Reggae"]'
+},
+	{
+		'title': "Brewery"
+		, 'q': "In which city can you find the Red Stripe Brewery in Jamaica?"
+		, 'a': "Kingston"
+		, 'choices': '[\"Manchester\",\"St. Ann\",\"Kingston\"]'
+    }
+	, {
+		'title': "Coco Bread"
+		, 'q': "What is another name for Coco Bread?"
+		, 'a': "Mongoose Bread"
+		, 'choices': '[\"Mongoose Bread\",\"Shortbread\",\"Janga Bread\"]'
+    }
+	, {
+		'title': "Jerk festival"
+		, 'q': "Which town hosts Western Jamaica’s largest Jerk Festival each year?"
+		, 'a': "Montego Bay"
+		, 'choices': '[\"Falmouth\",\"Montego Bay\",\"Green Island\"] '
+    }
+	, {
+		'title': "August days"
+		, 'q': "Emancipation Day and Independence Day are celebrated on which of the following dates?"
+		, 'a': "August 1st & August 6th"
+		, 'choices': '[\"July 10th & July 16th\",\"August 1st & August 6th\",\"September 6th and September 10th\"] '
+    }
+	, {
+		'title': "Bob Marley"
+		, 'q': "At what age did Bob Marley make his first album?"
+		, 'a': "16 years old"
+		, 'choices': '[\"16 years old\",\"19 years old\",\"21 years old\"]'
+    }
+	, {
+		'title': "Cubans"
+		, 'q': "Cubans described Jamaica as “The Land of Blessed Riches”.' "
+		, 'a': 'False'
+		, 'choices': '["True","False"]'
+    }
+	, {
+		'title': "Wick city"
+		, 'q': "Port Royal was once described as one of the “wickedest cities in the world”.'"
+		, 'a': 'True'
+		, 'choices': '["True","False"]'
+    }
+	, {
+		'title': "Blue Mtn"
+		, 'q': "Jamaica produces Blue Mountain Coffee. "
+		, 'a': 'True'
+		, 'choices': '["True","False"]'
+    }
+	, {
+		'title': "Original name"
+		, 'q': "Jamaica’s original name was Xaymaca, meaning, “Land of wood and water”.'"
+		, 'a': 'True'
+		, 'choices': '["True","False"]'
+    }
+	, {
+		'title': "Winter O"
+		, 'q': "In 1992 Jamaica participated in the winter Olympics. "
+		, 'a': 'False'
+		, 'choices': '["True","False"]'
+    }
+]
+var questions = [];
+for (var i = 0; i < docsheet.length; i++) {
+	questions[i] = docsheet[i];
+	questions[i]['choices'] = JSON.parse(docsheet[i]['choices']);
+}
