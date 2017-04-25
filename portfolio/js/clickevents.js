c950bd6b2ee313a1679057f10950b0ef6bf2244c
Jon Koller <jmk4411@gmail.com>
2017-04-25 15:21:03 -0400

    portfolio added

---
+++ portfolio/js/clickevents.js
@@ -0,0 +1,18 @@
+$('#threecm').click(function(){
+	window.location.href = "http://www.jonkoller.com/3cardmonte/main";
+});
+$('#cockquiz').click(function(){
+	window.location.href = "http://www.jonkoller.com/cocktail/main";
+})
+$('#msg').click(function(){
+	window.location.href = "http://www.jonkoller.com/greyCircle/untitled.html";
+})
+$('#rs_triv').click(function(){
+	window.location.href = "http://www.jonkoller.com/quiz/quiz.html";
+})
+$('#rs_wallp').click(function(){
+	window.location.href = "http://www.jonkoller.com/choose_	wallpaper/index";
+})
+$('#stelar').click(function(){
+	window.location.href = "http://www.jonkoller.com/9steps/index";
+})
