36065daeb8238fbaefe5df84f7f8f0bfc8492231
Jon Koller <jmk4411@gmail.com>
2017-05-12 16:42:23 -0400

    videos added to page

---
+++ vdex/resizeframe.js
@@ -0,0 +1,6 @@
+$('#vid1Title').click(function () {
+	$('iframe').attr('src', './video1_frame/video1_frame.htm');
+});
+$('#vid2Title').click(function () {
+	$('iframe').attr('src', './video2_frame/video2_frame.htm');
+});
