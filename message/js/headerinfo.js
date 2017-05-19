ee625e64dd396ee4f438ec9d2f4aca6f447ebe5b
Jon Koller <jmk4411@gmail.com>
2017-05-18 21:06:02 -0400

    miss detection re-aligned

--- message/js/headerinfo.js
+++ message/js/headerinfo.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	headerSize = $("header").height();
 	var secBodyH = $('body').height() - headerSize*2;
 	setTimeout(function(){
-		
+
 	$("#body").attr('height', secBodyH);
 	}, 1000)
 	console.log($('body').height())
