f7f149e2fd3a784dbe0175cae58ebb2a6f732927
Jon Koller <jmk4411@gmail.com>
2017-05-19 01:52:28 -0400

    going back tto the orignal

--- message/js/untitled.js
+++ message/js/untitled.js
@@ -13,102 +13,6 @@ var coordObj = function (l, t) {
 	$(window).on("orientationchange", function () {
 		location.reload();
 	});
-	//def later completed in getBackgroundSize
-	var background = {
-		width: 0
-		, height: 0
-	}
-
-	function getBackgroundSize(elem) {
-		var computedStyle = getComputedStyle(elem)
-			, image = new Image()
-			, src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-			, cssSize = computedStyle.backgroundSize
-			, elemW = parseInt(computedStyle.width.replace('px', ''), 10)
-			, elemH = parseInt(computedStyle.height.replace('px', ''), 10)
-			, elemDim = [elemW, elemH]
-			, computedDim = []
-			, ratio;
-		image.src = src;
-		ratio = image.width > image.height ? image.width / image.height : image.height / image.width;
-		// Split background-size properties into array
-		cssSize = cssSize.split(' ');
-		computedDim[0] = cssSize[0];
-		computedDim[1] = cssSize.length > 1 ? cssSize[1] : 'auto';
-		if (cssSize[0] === 'cover') {
-			if (elemDim[0] > elemDim[1]) {
-				if (elemDim[0] / elemDim[1] >= ratio) {
-					computedDim[0] = elemDim[0];
-					computedDim[1] = 'auto';
-				}
-				else {
-					computedDim[0] = 'auto';
-					computedDim[1] = elemDim[1];
-				}
-			}
-			else {
-				computedDim[0] = 'auto';
-				computedDim[1] = elemDim[1];
-			}
-		}
-		else if (cssSize[0] === 'contain') {
-			// Width is less than height
-			if (elemDim[0] < elemDim[1]) {
-				computedDim[0] = elemDim[0];
-				computedDim[1] = 'auto';
-			}
-			else {
-				// elem's ratio is greater than or equal to img ratio
-				if (elemDim[0] / elemDim[1] >= ratio) {
-					computedDim[0] = 'auto';
-					computedDim[1] = elemDim[1];
-				}
-				else {
-					computedDim[1] = 'auto';
-					computedDim[0] = elemDim[0];
-				}
-			}
-		}
-		else {
-			// If not 'cover' or 'contain', loop through the values
-			for (var i = cssSize.length; i--;) {
-				// Check if values are in pixels or in percentage
-				if (cssSize[i].indexOf('px') > -1) {
-					// If in pixels, just remove the 'px' to get the value
-					computedDim[i] = cssSize[i].replace('px', '');
-				}
-				else if (cssSize[i].indexOf('%') > -1) {
-					// If percentage, get percentage of elem's dimension
-					// and assign it to the computed dimension
-					computedDim[i] = elemDim[i] * (cssSize[i].replace('%', '') / 100);
-				}
-			}
-		}
-		// If both values are set to auto, return image's 
-		// original width and height
-		if (computedDim[0] === 'auto' && computedDim[1] === 'auto') {
-			computedDim[0] = image.width;
-			computedDim[1] = image.height;
-		}
-		else {
-			// Depending on whether width or height is auto,
-			// calculate the value in pixels of auto.
-			// ratio in here is just getting proportions.
-			ratio = computedDim[0] === 'auto' ? image.height / computedDim[1] : image.width / computedDim[0];
-			computedDim[0] = computedDim[0] === 'auto' ? image.width / ratio : computedDim[0];
-			computedDim[1] = computedDim[1] === 'auto' ? image.height / ratio : computedDim[1];
-		}
-		// Finally, return an object with the width and height of the
-		// background image.
-		return {
-			width: computedDim[0]
-			, height: computedDim[1]
-		};
-	}
-	var background = {
-		width: ""
-		, height: ""
-	}
 	var score = 0;
 	var hitspots;
 
@@ -170,8 +74,8 @@ var coordObj = function (l, t) {
 	}
 ];
 		$(".score").html("0 of " + hotspots.length + " found");
-		(function createHotspots() {
-			//		alert(iOS);
+
+
 			for (var i = 0; i < hotspots.length; i++) {
 				$("#container").prepend('<div class="hit" id="hit' + i + '"/>');
 				$("#hit" + i).css("top", hotspots[i].top + "px");
@@ -179,7 +83,7 @@ var coordObj = function (l, t) {
 				$("#hit" + i).css("width", circleWidth + "px");
 				$("#hit" + i).css("height", circleWidth + "px");
 			};
-		})();
+
 		hitspots = document.getElementsByClassName("hit");
 		for (var i = 0; i < hitspots.length; i++) {
 			hitCoordObjs[i] = new coordObj($('.hit')[i].offsetLeft, $('.hit')[i].offsetTop);
@@ -188,7 +92,8 @@ var coordObj = function (l, t) {
 			var id = hitspots[i].getAttribute("id");
 		}
 		var index = 1;
-		$(".hit").on("click", function (event) {
+		
+		$(".hit").on("tap", function (event) {
 			clickedBool = true;
 				$("#" + this.getAttribute("id")).css("animation", "border .15s ease 1 forwards");
 				$('#bottle' + index).css('opacity', '1');				
@@ -226,18 +131,25 @@ document.getElementById("body").style.width = w;
 	})();
 	(function alignCanvas() {
 		$("#body").attr("width", w + "px");
-		$("#body").attr("height", h + "px");
+		$("#body").attr("width", w + "px");
+		$("#myCanvas").attr("height", h + "px");
+		$("#myCanvas").attr("height", h + "px");
 	})();
 	var touchzone = document.getElementById("body");
-	(function setupMissedClicks() {
-		touchzone.addEventListener("touchstart", getPageVals, false);
-		touchzone.addEventListener("touchend", drawCircle, false);
-	})();
+//	(function setupMissedClicks() {
+//		touchzone.addEventListener("touchstart", getPageVals, false);
+//		touchzone.addEventListener("touchend", drawCircle, false);
+		$('#body').on('tap',function(){
+			if(clickedBool == false){
+				drawCircle;
+			}
+		});
+//	})();
 	var pagX, pagY;
 	var compareObj;
 	$("#container").click(function (event) {
 		compareObj = new coordObj(event.offsetLeft, event.offsetTop);
-		console.log(compareObj);
+//		console.log(compareObj);
 	})
 
 	function getPageVals(event) {
