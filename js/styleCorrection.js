41df7082e9def90675161ad12e74edeb94b19e16
Jon Koller <jmk4411@gmail.com>
2017-07-05 17:43:45 -0400

    ll

--- js/styleCorrection.js
+++ js/styleCorrection.js
@@ -1,25 +1,6 @@
 var resizeEvent = new Event('resize');
 var portfolio = document.getElementById('portMenu');
-//$('.portfolio').click(function () {
-//	$('#portfolio').css('visibility', 'visible');
-//	$('#portfolio').children().css('visibility', 'visible');
-//	$('#portfolio').children().children().css('visibility', 'visible');
-//	portfolio.scrollIntoView();
-//})
-//var about = document.getElementById('about');
-//$('.blue').click(function () {
-//	$('#about').css('visibility', 'visible');
-//	$('#about').children().css('visibility', 'visible');
-//	$('#about').children().children().css('visibility', 'visible');
-//	$('#about').children().css('display', 'block');
-//	$('#about').css('display', 'block');
-//	$('#about').css('width', '100%');
-//	$('#about').css('padding', '30% 70% 3% 3%');
-//	about.scrollIntoView({
-//		block: "start"
-//		, behavior: "instant"
-//	});
-//})
+
 var i, buttons, classGame;
 document.addEventListener("DOMContentLoaded", function (event) {
 	buttons = document.getElementsByClassName('button');
