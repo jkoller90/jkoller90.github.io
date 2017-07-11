bac80d4fe08114196f4112e2e5ea49174cbe8afe
jkoller3 <koller@fordham.edu>
2017-07-11 09:02:00 -0400

    fixing resumes. about menu shows

--- js/styleCorrection.js
+++ js/styleCorrection.js
@@ -4,48 +4,60 @@ var about = document.getElementById('aboutMenu');
 var i, buttons, classGame, classWriting, classAbout;
 var portFlag = false;
 document.addEventListener("DOMContentLoaded", function (event) {
-	swal("This site is in development; please be careful with resizing the page! Formatting is underway.");
-	buttons = document.getElementsByClassName('button');
-	classGame = document.getElementsByClassName('game');
-	for (i = 0; i < buttons.length; i++) {
-		buttons[i].onmouseover = function () {
-			this.style.backgroundColor = "antiquewhite";
-		}
-		buttons[i].onmouseout = function () {
-			this.style.backgroundColor = 'white';
-		}
-		buttons[i].onmousedown = function () {
-			this.style.backgroundColor = "antiquewhite";
-			if (this.id == 'aboutButton') {
-				for (i = 0; i < classGame.length; i++) {
-					classGame[i].style.display = 'none';
-				}
-				portfolio.style.display = 'none';
-				swal('Under development!');
-			}
-			else if (this.id == 'cvButton') {
-				for (i = 0; i < classGame.length; i++) {
-					classGame[i].style.display = 'none';
-				}
-				portfolio.style.display = 'none';
-				swal('Under development!');
-			}
-			else if (this.id == 'portButton') {
-				if (!portFlag) {
-					portFlag = true;
-					for (i = 0; i < classGame.length; i++) {
-						classGame[i].style.display = 'block';
-					}
-					portfolio.style.display = 'inline';
-				}
-				else {
-					portFlag = false;
-					for (i = 0; i < classGame.length; i++) {
-						classGame[i].style.display = 'none';
-					}
-					portfolio.style.display = 'none';
-				}
-			}
-		}
-	}
+  //	swal("This site is in development; please be careful with resizing the page! Formatting is underway.");
+  buttons = document.getElementsByClassName('button');
+  classGame = document.getElementsByClassName('game');
+  classAbout = document.getElementsByClassName('classAbout');
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].onmouseover = function () {
+      this.style.backgroundColor = "antiquewhite";
+    }
+    buttons[i].onmouseout = function () {
+      this.style.backgroundColor = 'white';
+    }
+    buttons[i].onmousedown = function () {
+      this.style.backgroundColor = "antiquewhite";
+      if (this.id == 'aboutButton') {
+        for (i = 0; i < classGame.length; i++) {
+          classGame[i].style.display = 'none';
+        }
+        portfolio.style.display = 'none';
+        about.style.display = 'inline';
+        if (!portFlag) {
+          portFlag = true;
+          for (i = 0; i < classAbout.length; i++) {
+            classAbout[i].style.display = 'block';
+          }
+          portfolio.style.display = 'inline';
+        } else {
+          portFlag = false;
+          for (i = 0; i < classAbout.length; i++) {
+            classAbout[i].style.display = 'none';
+          }
+          about.style.display = 'none';
+        }
+      } else if (this.id == 'cvButton') {
+        for (i = 0; i < classGame.length; i++) {
+          classGame[i].style.display = 'none';
+        }
+        portfolio.style.display = 'none';
+        swal('Under development!');
+      } else if (this.id == 'portButton') {
+        if (!portFlag) {
+          portFlag = true;
+          for (i = 0; i < classGame.length; i++) {
+            classGame[i].style.display = 'block';
+          }
+          about.style.display = 'none';
+          portfolio.style.display = 'inline';
+        } else {
+          portFlag = false;
+          for (i = 0; i < classGame.length; i++) {
+            classGame[i].style.display = 'none';
+          }
+          portfolio.style.display = 'none';
+        }
+      }
+    }
+  }
 });
