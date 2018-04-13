82991dc83894e7dc7b77d2e352ad10cf990dc447
jkoller90 <jmk4411@gmail.com>
2018-04-13 04:10:26 -0400

    trimmed project size by a lot

--- portfolio/cocktail/js/setCongrats.js
+++
@@ -1,5 +0,0 @@
-function setCongrats() {
-	$('.congrats').css('display', 'block');
-//	$('#earned').text('You just earned '+score+' points by answering 4 questions correctly.');
-		$('#earned').text('You just earned '+roundScore+' points by answering 4 questions correctly.');
-}
--- portfolio/message/js/headerinfo.js
+++
@@ -1,9 +0,0 @@
-var headerSize;
-$(document).ready(function () {
-	headerSize = $("header").height();
-	var secBodyH = $('body').height() - headerSize * 2;
-	setTimeout(function () {
-			//	$("#body").attr('height', secBodyH);
-		}, 1000)
-
-})
